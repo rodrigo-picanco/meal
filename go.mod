33dbbaefcd9b253771848309bd216263964986e0
Rodrigo Picanco Tavares <rodrigopicancotavares@gmail.com>
2024-09-20 20:52:34 +0200

    lexer boilerplate

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module meal
 
 go 1.23.1
 
-require gopkg.in/yaml.v3 v3.0.1 // indirect
+require (
+	github.com/alecthomas/participle v0.7.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- meal.go
+++ meal.go
@@ -1,82 +1,135 @@
 package main
 
 import (
-        "fmt"
-        "gopkg.in/yaml.v3"
-        "log"
-        "errors"
-        "os"
+	"io/ioutil"
+	"os"
 )
 
-type Ingredient struct {
-        Name string
-        Amount int
-}
-
-type Recipe struct {
-        Name string
-        Ingredients []Ingredient
-}
-
-type Day struct {
-        Day string
-        Recipes []string
-}
-
-type DSL struct {
-        Plan []Day
-        Recipes []Recipe
-}
-
 func main() {
-        data, err := os.ReadFile(os.Args[1])
-        if err != nil {
-                log.Fatalf("reading error: %v", err)
-        }
-
-        result := DSL{}
-        err = yaml.Unmarshal([]byte(data), &result)
-        if err != nil {
-                log.Fatalf("parsing error: %v", err)
-        }
-
-        fmt.Println("Shopping List:")
-        list := make(map[string]int)
-        notFound := make([]string, 0)
-        for _, day := range result.Plan {
-                for _, recipe := range day.Recipes {
-                        recipe, err := findRecipe(result.Recipes, recipe)
-
-                        if err != nil{
-                                notFound = append(notFound, recipe.Name)
-                                continue
-                        }
-
-                        for _, ingredient := range recipe.Ingredients {
-                                list[ingredient.Name] += ingredient.Amount
-                        }
-                }
-        }
-
-        for ingredient, amount := range list {
-                fmt.Println("  ", ingredient, amount)
-        }
-
-        if len(notFound) > 0 {
-                fmt.Println("Recipes not found:")
-                for _, recipe := range notFound {
-                        fmt.Println("  ", recipe)
-                }
-        }
-
-
+	input, error := ioutil.ReadFile(os.Args[1])
+	if error != nil {
+		println("Error reading file")
+		os.Exit(1)
+	}
+	l := Lexer{input: string(input)}
+	l.readChar()
+	for {
+		tok := l.NextToken()
+		if tok.Type == ILLEGAL {
+			break
+		}
+		println(tok.Type, tok.Literal)
+	}
 }
 
-func findRecipe(recipes []Recipe, name string) (*Recipe, error) {
-        for _, recipe := range recipes {
-                if recipe.Name == name {
-                        return &recipe, nil
-                }
-        }
-        return &Recipe{Name: name}, errors.New("Recipe not found: " + name)
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	IDENT   = "IDENT"
+	COMMA   = ","
+	LPAREN  = "("
+	RPAREN  = ")"
+	INT     = "INT"
+	WEEKDAY = "WEEKDAY"
+	UNIT    = "UNIT"
+)
+
+type Lexer struct {
+	input        string
+	position     int
+	readPosition int
+	ch           byte
+}
+
+var keywords = map[string]TokenType{
+        "monday": WEEKDAY,
+        "tuesday": WEEKDAY,
+        "wednesday": WEEKDAY,
+        "thursday": WEEKDAY,
+        "friday": WEEKDAY,
+        "saturday": WEEKDAY,
+        "sunday": WEEKDAY,
+}
+
+func (l *Lexer) NextToken() Token {
+	var tok Token
+	l.skipWhitespace()
+
+	switch l.ch {
+	case ',':
+		tok = newToken(COMMA, l.ch)
+	case '(':
+		tok = newToken(LPAREN, l.ch)
+	case ')':
+		tok = newToken(RPAREN, l.ch)
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			if t, ok := keywords[tok.Literal]; ok {
+                                tok.Type = t
+			} else {
+                                tok.Type = IDENT
+                        }
+
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Type = INT
+			tok.Literal = l.readNumber()
+			return tok
+		} else {
+			tok = newToken(ILLEGAL, l.ch)
+		}
+	}
+	l.readChar()
+	return tok
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+func newToken(tokenType TokenType, ch byte) Token {
+	return Token{Type: tokenType, Literal: string(ch)}
 }
